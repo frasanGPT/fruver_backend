dc845598f44add1fd4adf4207d5f97b6e2dc2578
Franklyn <frasan1@gmail.com>
2026-02-19 20:22:00 -0500

    add AuditLog model

---
+++ src/models/AuditLog.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const auditLogSchema = new mongoose.Schema(
+  {
+    usuarioId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: true,
+    },
+    usuarioEmail: {
+      type: String,
+      required: true,
+    },
+    accion: {
+      type: String,
+      required: true,
+    },
+    entidad: {
+      type: String,
+      required: true,
+    },
+    entidadId: {
+      type: String,
+    },
+    metadata: {
+      type: Object,
+    },
+    ip: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const AuditLog = mongoose.model("AuditLog", auditLogSchema);
+
+export default AuditLog;
